ac8e7e1d83d9e2442bf30b3068c601c31c33ecc7
Aleksei Mikishev <avmiki80@mail.ru>
2020-12-14 17:39:19 +0500

    отключил security

---
+++ src/main/java/ru/avid/scheduler/auth/config/SpringConfig.java
@@ -0,0 +1,17 @@
+package ru.avid.scheduler.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+public class SpringConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin().disable();
+        http.httpBasic().disable();
+        http.requiresChannel().anyRequest().requiresSecure();
+    }
+}
